80f4e1e51e4ce86e7eafbe5fe74c68b368439d54
Arthur <o6art7@gmail.com>
2021-10-18 16:44:28 +0200

    Add ComToDataInterface

--- src/main/java/nomad/common/interfaces/com/ComToDataInterface.java
+++ src/main/java/nomad/common/interfaces/com/ComToDataInterface.java
@@ -1,5 +1,29 @@
 package nomad.common.interfaces.com;
 
+import nomad.common.data_structure.Game;
+import nomad.common.data_structure.Move;
+import nomad.common.data_structure.Player;
+import nomad.common.data_structure.User;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ComToDataInterface {
-    public void createGame(model.Game game);
+    void createGame(Game game);
+    Game getGame(UUID gameId);
+    List<User> getUserList(UUID gameId);
+    Move saveMove(UUID playerId, Move move);
+    boolean checkAction(Move action, UUID clientId);
+    void storeAction(Move action, UUID clientId);
+    void updateGame();
+    Game getStoredGame(UUID gameId);
+    List<User> requestUserList();
+    List<Game> requestGameList();
+    void updateUserList(User newUser);
+    void updateUserList(UUID userId);
+    void joinGameRequest(Player player, Game game);
+    void guestAccepted(Player player);
+    void storeSavedGame(Game savedGame);
+    void getProfileInfos(UUID userId);
+    void getAvailableStoredGames();
 }
