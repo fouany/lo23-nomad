5700a725960fe9777c98e09d335b872b901a8a65
Hugo Lecourt <hugo.lecourt@etu.utc.fr>
2021-11-08 14:15:21 +0000

    Fix/Com interface

--- src/main/java/nomad/common/interfaces/com/ComToIhmGameInterface.java
+++ src/main/java/nomad/common/interfaces/com/ComToIhmGameInterface.java
@@ -1,15 +1,9 @@
 package nomad.common.interfaces.com;
 
+import nomad.common.data_structure.GameLight;
 import nomad.common.data_structure.Message;
-import nomad.common.data_structure.Skip;
-import nomad.common.data_structure.Tile;
-
-import java.util.UUID;
 
 public interface ComToIhmGameInterface {
-    void sendNewPlayer(UUID userId);
-    void endGame(UUID idGame, boolean gameEnded);
-    void sendMessage(Message msg);
-    void isValid(Tile m, boolean valid);
-    void isValid(Skip s, boolean valid);
+    void checkGameEnd(GameLight game);
+    void transmissionCom(Message msg);
 }
