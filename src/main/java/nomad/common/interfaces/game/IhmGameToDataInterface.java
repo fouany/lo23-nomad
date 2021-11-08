81c088c919ce6913f4189727e1d336d6316a4288
Valentin Mention <valentin.mention@etu.utc.fr>
2021-11-08 12:50:26 +0000

    Merge "Declare game interfaces" into develop

--- src/main/java/nomad/common/interfaces/game/IhmGameToDataInterface.java
+++ src/main/java/nomad/common/interfaces/game/IhmGameToDataInterface.java
@@ -1,4 +1,17 @@
 package nomad.common.interfaces.game;
 
+import nomad.common.data_structure.Game;
+
+/**
+ * Expose methods of the {@code nomad.game} package to {@code nomad.data}.
+ *
+ * <br>Expose the {@code updateObservable} methods to acknowledge changes on observed objects.
+ */
 public interface IhmGameToDataInterface {
+
+    /**
+     * Update {@code Game} observed object when changed.
+     * @param game The {@code Game} object to be updated by data module.
+     */
+    void updateObservable(Game game);
 }
