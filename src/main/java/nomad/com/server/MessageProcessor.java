abab097ed06857bd7bfafac449660728b26f3a16
Valentin Mention <valentin.mention@etu.utc.fr>
2021-11-22 13:27:15 +0100

    Document MessageProcessor

--- src/main/java/nomad/com/server/MessageProcessor.java
+++ src/main/java/nomad/com/server/MessageProcessor.java
@@ -1,9 +1,6 @@
 package nomad.com.server;
 
-import nomad.com.common.message.ComMessage;
-import nomad.com.common.message.LobbyInformationMessage;
-import nomad.com.common.message.LocalUserConnectionMessage;
-import nomad.com.common.message.UserChangedMessage;
+import nomad.com.common.message.*;
 import nomad.common.data_structure.GameLight;
 import nomad.common.data_structure.Player;
 import nomad.common.interfaces.data.DataToComServerInterface;
@@ -14,17 +11,31 @@ import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * MessageProcessor is responsible of processing all messages received by the server
+ */
 public class MessageProcessor {
     private final DataToComServerInterface dataToCom;
     private final Server server;
 
+    /**
+     *
+     * @param dataToCom Interface between Com and Data
+     * @param server Server object reference
+     */
     public MessageProcessor(DataToComServerInterface dataToCom, Server server) {
         this.dataToCom = dataToCom;
         this.server = server;
     }
 
+    /**
+     * Process a message received from a client
+     *
+     * @param socket Send client socket
+     * @param message Sent message
+     */
     public void processMessage(Socket socket, ComMessage message) {
-        Logger.getLogger(this.getClass().getName()).log(Level.INFO, "Received message type : " + message.getClass().toString());
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, message.getClass().toString());
         UUID userId = server.getAssociatedUser(socket);
 
         if (userId == null && message.getClass() == LocalUserConnectionMessage.class) {
@@ -33,20 +44,35 @@ public class MessageProcessor {
         }
 
         Class<? extends ComMessage> messageClass = message.getClass();
+        if (messageClass.equals(LocalUserDisconnectionMessage.class)) {
+
+        }
     }
 
+    /**
+     * Register a new connected client on the server
+     *
+     * @param socket Send client socket
+     * @param message Connection message
+     */
     private void registerUser(Socket socket, LocalUserConnectionMessage message) {
         server.broadcast(new UserChangedMessage(message.user, true));
         dataToCom.updateUserListAdd(message.user);
         server.registerUser(socket, message.user);
     }
 
+    /**
+     * Send informations on games and users to newly connected client
+     *
+     * @param socket Socket of new client
+     */
     private void sendLobbyInformation(Socket socket) {
         LobbyInformationMessage informationMessage = new LobbyInformationMessage(
                 (ArrayList<GameLight>) dataToCom.requestGameList(),
                 (ArrayList<Player>) dataToCom.requestConnectedUserList()
         );
-
         server.sendMessage(socket, informationMessage);
     }
+
+
 }
