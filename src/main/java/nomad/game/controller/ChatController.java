b255a700f3c665c813c2ee932593bb77b725057c
alicemainguet <50203696+alicemainguet@users.noreply.github.com>
2021-11-29 14:30:14 +0100

    update chatController

---
+++ src/main/java/nomad/game/controller/ChatController.java
@@ -0,0 +1,14 @@
+package nomad.game.controller;
+
+import nomad.common.ihm.IhmControllerComponent;
+import nomad.common.data_structure.Game;
+import nomad.common.ihm.IhmScreenController;
+import nomad.game.IhmGameScreenController;
+
+public class ChatController extends IhmControllerComponent {
+
+    public ChatController(IhmGameScreenController screen) {
+        super(screen);
+
+    }
+}
