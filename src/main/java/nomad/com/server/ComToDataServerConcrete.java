76c5361111f2f3549a065c0aa69a5e91b339e8b5
Hugo Lecourt <hlecourt99@gmail.com>
2021-11-29 14:28:22 +0100

    Fix pipeline

--- src/main/java/nomad/com/server/ComToDataServerConcrete.java
+++ src/main/java/nomad/com/server/ComToDataServerConcrete.java
@@ -1,44 +1,31 @@
 package nomad.com.server;
 
-import nomad.com.common.message.clientMessage.NewGameUserMessage;
-import nomad.common.data_structure.Game;
-import nomad.common.data_structure.GameLight;
 import nomad.common.data_structure.Player;
+import nomad.common.data_structure.Skip;
+import nomad.common.data_structure.Tile;
+import nomad.common.data_structure.Tower;
 import nomad.common.interfaces.com.ComToDataServerInterface;
 
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 public class ComToDataServerConcrete implements ComToDataServerInterface {
-    ServerController serverController;
+    //ServerController serverController;
 
     @Override
     public void requestHost(Player player) {
-        HashMap<Socket, IdentifiedClient> clients = serverController.getClientList();
-        IdentifiedClient client = null;
-        for (IdentifiedClient cli : clients.values()) {
-            if (cli.getUID() == player.getId()) {
-                client = cli;
-                break;
-            }
-        }
-        if (client == null) {
-            throw new NullPointerException();
-        }
-        // TODO : comment mieux récupérer le GameLight ici ?
-        List<GameLight> games = serverController.getDataToCom().requestGameListInLobby();
-        GameLight game = null;
-        for (GameLight g : games) {
-            if (g.getHost().getId() == player.getId()) {
-                game = g;
-            }
-        }
+        //TODO
+    }
 
-        if (game == null) {
-            throw new NullPointerException();
-        }
-        serverController.sendMessage(client.getSocket(), new NewGameUserMessage(game, player, true));
+    @Override
+    public void towerValid(Tower t) {
+        //TODO
+    }
+
+    @Override
+    public void tileValid(Tile t) {
+        //TODO
+    }
+
+    @Override
+    public void skipValid(Skip s) {
+        //TODO
     }
 }
--- src/main/java/nomad/common/interfaces/com/ComToDataServerInterface.java
+++ src/main/java/nomad/common/interfaces/com/ComToDataServerInterface.java
@@ -1,7 +1,16 @@
 package nomad.common.interfaces.com;
 
 import nomad.common.data_structure.Player;
+import nomad.common.data_structure.Skip;
+import nomad.common.data_structure.Tile;
+import nomad.common.data_structure.Tower;
 
 public interface ComToDataServerInterface {
     void requestHost(Player player);
+
+    void towerValid(Tower t);
+
+    void tileValid(Tile t);
+
+    void skipValid (Skip s);
 }
