94a183c7e4b384d060025abc1e6a77f848e0a2e4
Hugo Lecourt <hlecourt99@gmail.com>
2021-11-29 15:54:35 +0100

    Update ComToIhmGameInterface.java

--- src/main/java/nomad/common/interfaces/com/ComToIhmGameInterface.java
+++ src/main/java/nomad/common/interfaces/com/ComToIhmGameInterface.java
@@ -2,9 +2,12 @@ package nomad.common.interfaces.com;
 
 import nomad.common.data_structure.GameLight;
 import nomad.common.data_structure.Message;
+import nomad.common.data_structure.Move;
 
 public interface ComToIhmGameInterface {
     void checkGameEnd(GameLight game);
 
     void transmissionCom(Message msg);
+
+    void playMove(Move move);
 }
