94936fcd5308d16e822b2bd010c2eef21873d781
Hiba <hiba.hammi@etu.utc.fr>
2021-12-12 01:39:52 +0100

    added controller for information viewer

---
+++ src/main/java/nomad/game/controller/InfoViewerController.java
@@ -0,0 +1,24 @@
+package nomad.game.controller;
+
+import nomad.common.ihm.IhmScreenController;
+
+public class InfoViewerController extends GameComponentsAbstract {
+    /**
+     * Constructor that link the screen controller to the component controller
+     *
+     * @param screen
+     */
+    protected InfoViewerController(IhmScreenController screen) {
+        super(screen);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update(String type, Object value) {
+    }
+}
+
