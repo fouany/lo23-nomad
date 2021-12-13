0d7708a9ba70f8b205ce516d3ee38677e30a00e8
Gabriel Francez <gabriel.francez@etu.utc.fr>
2021-12-13 13:01:28 +0000

    Import profile

--- src/main/java/nomad/main/controller/LoginController.java
+++ src/main/java/nomad/main/controller/LoginController.java
@@ -3,8 +3,15 @@ package nomad.main.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import nomad.common.data_structure.UserException;
 import nomad.common.ihm.IhmControllerComponent;
 import nomad.main.IhmMainScreenController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LoginController extends IhmControllerComponent {
     @FXML
@@ -21,8 +28,21 @@ public class LoginController extends IhmControllerComponent {
         this.isSignup = false;
     }
 
+    FileChooser fileChooser = new FileChooser();
+
     public void onClickImportProfile() {
         // TODO : implement method
+        fileChooser.setTitle("Import profile");
+        File file = fileChooser.showOpenDialog(this.ihmMainScreenController.getStage());
+        try {
+            ihmMainScreenController.getDataI().addAccount(file.getPath());
+        } catch (IOException e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, e.getMessage());
+        } catch (ClassNotFoundException e) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, e.getMessage());
+        } catch (UserException e) {
+            DialogController.display("Erreur","L'utilisateur existe déjà en local.", DialogController.DialogStatus.ERROR, ihmMainScreenController);
+        }
     }
 
     public void onClickSignUp() {
