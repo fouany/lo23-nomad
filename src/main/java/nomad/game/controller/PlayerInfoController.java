cd063f528c887e6f91112a6b746a788db2650379
Valentin Mention <valentin.mention@etu.utc.fr>
2021-12-31 13:04:23 +0100

    Fix JavaFX thread exception

--- src/main/java/nomad/game/controller/PlayerInfoController.java
+++ src/main/java/nomad/game/controller/PlayerInfoController.java
@@ -1,23 +1,14 @@
 package nomad.game.controller;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import nomad.common.data_structure.Game;
 import nomad.common.ihm.IhmScreenController;
 
-import java.util.UUID;
 
 public class PlayerInfoController extends GameControllerAbstract {
-
-    private GameController gameController;
-
-    /**
-     * Constructor that link the screen controller to the component controller
-     *
-     * @param screen
-     */
 
     @FXML
     public Label playerOneName;
@@ -37,46 +28,41 @@ public class PlayerInfoController extends GameControllerAbstract {
     @FXML
     public Label numberTiles;
 
-    private UUID currentUser;
-
-    private IhmScreenController gameScreen;
-
-
+    /**
+     * Constructor that link the screen controller to the component controller
+     *
+     * @param screen Parent screen controller
+     */
     public PlayerInfoController(IhmScreenController screen) {
         super(screen);
-        gameScreen = screen;
     }
 
     @Override
     public void init() {
         Game game = getGameController().getCurrentGame();
-        currentUser = getGameController().getGameScreen().getDataInterface().getUser().getUserId();
-        playerOneName.setText("Player: "+game.getHost().getLogin());
+        playerOneName.setText("Player: " + game.getHost().getLogin());
         playerTwoName.setText("Player: " + game.getOpponent().getLogin());
-        numberTower.setText("Towers : "+game.getNbOfTowers()+"/5");
-        numberTiles.setText("Tiles : "+game.getNbOfTilesPlayed()+"/5");
+        numberTower.setText("Towers : " + game.getNbOfTowers() + "/5");
+        numberTiles.setText("Tiles : " + game.getNbOfTilesPlayed() + "/5");
         updatePane(game);
-        System.out.println("Start player :"+game.getCurrentPlayerUUID());
     }
 
     public void update() {
         Game game = getGameController().getCurrentGame();
-//      True if it's your turn game.getCurrentPlayerUUID().equals(currentUser);
         updatePane(game);
-        numberTower.setText("Towers : "+game.getNbOfTowers()+"/5");
-        numberTiles.setText("Tiles : "+game.getNbOfTilesPlayed()+"/5");
+        Platform.runLater(() -> {
+            numberTower.setText("Towers : " + game.getNbOfTowers() + "/5");
+            numberTiles.setText("Tiles : " + game.getNbOfTilesPlayed() + "/5");
+        });
     }
 
-    public void updatePane(Game game){
-        System.out.println(game.getCurrentPlayerUUID());
-        System.out.println(game.getHost().getId());
-        if(game.getCurrentPlayerUUID().equals(game.getHost().getId())){
+    public void updatePane(Game game) {
+        if (game.getCurrentPlayerUUID().equals(game.getHost().getId())) {
             paneUser1.setStyle("-fx-background-color: #134900;");
             paneUser2.setStyle("-fx-background-color: #003049;");
-        }else{
-            paneUser2.setStyle("-fx-background-color: #134900;");
+        } else {
             paneUser1.setStyle("-fx-background-color: #003049;");
+            paneUser2.setStyle("-fx-background-color: #134900;");
         }
     }
-
 }
